793a0ee7b835260cc05961fe51701c042fefb522
Peter Khalil <uni.03c9@gmail.com>
2022-03-30 22:52:12 +0200

    footer comp

--- src/components/footer/Footer.comp.jsx
+++ src/components/footer/Footer.comp.jsx
@@ -1,11 +1,38 @@
 import React from "react";
+import { FiInstagram, FiTwitter, FiFacebook } from "react-icons/fi";
+
 import "./footer.styles.css";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer>
+      <a href="#" className="footer__logo">
+        u03c9
+      </a>
+      <ul className="permalinks">
+        <a href="#">Home</a>
+        <a href="#about">About</a>
+        <a href="#experience">Experience</a>
+        <a href="#testimonials">Testimonials</a>
+        <a href="#contact">Contact</a>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com" target="_blank">
+          <FiFacebook />
+        </a>
+        <a href="https://instagram.com" target="_blank">
+          <FiInstagram />
+        </a>
+        <a href="https://twitter.com" target="_blank">
+          <FiTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyrights">
+        <small>&copy; Just Kidding</small>
+      </div>
+    </footer>
   );
 };
 
