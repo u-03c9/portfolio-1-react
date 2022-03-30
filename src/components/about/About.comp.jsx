f7f37af6e04f8f523281ffa45ebcc3af37148aa6
Peter Khalil <uni.03c9@gmail.com>
2022-03-30 16:41:24 +0200

    about comp

--- src/components/about/About.comp.jsx
+++ src/components/about/About.comp.jsx
@@ -1,10 +1,52 @@
 import React from "react";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
+
 import "./about.styles.css";
 
 const About = () => {
   return (
     <section id="about">
-      <h1>About</h1>
+      <h5>Get To Know</h5>
+      <h2>About me</h2>
+      <div className="container about__container">
+        <div className="about__profile">
+          <div className="about__profile__pic_container">
+            <img src="/assets/me-about.jpg" alt="" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea,
+            voluptates inventore commodi explicabo nesciunt ratione vitae
+            nostrum! Sed, dolorem saepe? Accusamus accusantium assumenda
+            doloribus totam necessitatibus blanditiis nam veniam ex.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
