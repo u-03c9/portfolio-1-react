b4f08b14b1a8da2fc7f0c1cc35cd10ab2efc796b
Peter Khalil <uni.03c9@gmail.com>
2022-03-30 16:10:34 +0200

    nav comp

--- src/components/nav/Nav.comp.jsx
+++ src/components/nav/Nav.comp.jsx
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
+import { BiBook, BiMessageDetail } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+
 import "./nav.styles.css";
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
+
+  const isActive = (location) => (activeNav === location ? "active" : "");
+
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <nav>
+      <a href="#" className={isActive("#")} onClick={() => setActiveNav("#")}>
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        className={isActive("#about")}
+        onClick={() => setActiveNav("#about")}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experiences"
+        className={isActive("#experiences")}
+        onClick={() => setActiveNav("#experiences")}
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#services"
+        className={isActive("#services")}
+        onClick={() => setActiveNav("#services")}
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#contact"
+        className={isActive("#contact")}
+        onClick={() => setActiveNav("#contact")}
+      >
+        <BiMessageDetail />
+      </a>
+    </nav>
   );
 };
 
